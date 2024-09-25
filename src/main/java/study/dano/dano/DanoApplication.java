0a5353894a41e03f31405d3546ead12b01d601df
Kim-AYoung <onionlily123@naver.com>
2024-09-25 17:13:21 +0900

    feat: 초기 환경 설정

---
+++ src/main/java/study/dano/dano/DanoApplication.java
@@ -0,0 +1,13 @@
+package study.dano.dano;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/study/dano/dano/DanoApplicationTests.java
@@ -0,0 +1,13 @@
+package study.dano.dano;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
